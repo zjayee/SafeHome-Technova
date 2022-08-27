c66436c2e82344a28b3f737416618fad7d6f982d
ZJayee <jayee.zheng@gmail.com>
2022-08-27 00:19:28 -0700

    example person populated to help list

--- app/src/main/java/com/example/technova/DataStorage.java
+++ app/src/main/java/com/example/technova/DataStorage.java
@@ -16,12 +16,16 @@ public class DataStorage {
     static void populate_needList()
     {
         //TODO: Create people objects and add to needList
+
     }
 
     //adds people objects to help list
     static void populate_helpList()
     {
         //TODO: Create people objects and add to helpList
+        //Example: upload/drag cover photo and pfp to drawable folder under res to use
+        People person = new People("Jane", 16, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum accumsan ante ac sem pellentesque, a blandit metus cursus. ", R.drawable.pfp1, R.drawable.cover1);
+        helpList.add(person);
     }
 }
 
--- app/src/main/java/com/example/technova/People.java
+++ app/src/main/java/com/example/technova/People.java
@@ -6,16 +6,20 @@ import java.util.HashMap;
 public class People {
     String name;
     Integer age;
+    Integer profilePicture; //drawables can be stored as integers
+    Integer homeCoverPicture;
     String bio;
     HashMap<String, Boolean> preferences; //eg "kids-allowed":true
     //TODO: decide how to store location or if we want to add this feature
     //Add more properties depending on what info is in profile
 
     //constructor
-    People(String name, Integer age, String bio){
+    People(String name, Integer age, String bio, Integer profilePicture, Integer homeCoverPicture){
         this.name = name;
         this.age = age;
         this.bio = bio;
+        this.profilePicture = profilePicture;
+        this.homeCoverPicture = homeCoverPicture;
         preferences = new HashMap<>();
     }
 
