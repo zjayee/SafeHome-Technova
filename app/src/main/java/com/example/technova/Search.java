5ff35b0cafecb88719372f05f89c23630b0463fc
ZJayee <jayee.zheng@gmail.com>
2022-08-26 23:55:58 -0700

    Added search bar fragment layout

---
+++ app/src/main/java/com/example/technova/Search.java
@@ -0,0 +1,49 @@
+package com.example.technova;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Search#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Search extends Fragment {
+    View rootView;
+
+    public Search() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Search.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Search newInstance(String param1, String param2) {
+        Search fragment = new Search();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        return  rootView;
+    }
+}
